d6e1b22db5160fbe9da09b22a86c782863d427e7
DESKTOP-ACDD6TV\User <guewoljohannkionghat@gmail.com>
2024-05-27 10:27:02 +0200

    first commit

---
+++ src/Entity/Book.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $publishedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeImmutable
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeImmutable $publishedAt): static
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+}
---
+++ src/Factory/BookFactory.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<Book>
+ *
+ * @method        Book|Proxy                     create(array|callable $attributes = [])
+ * @method static Book|Proxy                     createOne(array $attributes = [])
+ * @method static Book|Proxy                     find(object|array|mixed $criteria)
+ * @method static Book|Proxy                     findOrCreate(array $attributes)
+ * @method static Book|Proxy                     first(string $sortedField = 'id')
+ * @method static Book|Proxy                     last(string $sortedField = 'id')
+ * @method static Book|Proxy                     random(array $attributes = [])
+ * @method static Book|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static BookRepository|RepositoryProxy repository()
+ * @method static Book[]|Proxy[]                 all()
+ * @method static Book[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Book[]|Proxy[]                 createSequence(iterable|callable $sequence)
+ * @method static Book[]|Proxy[]                 findBy(array $attributes)
+ * @method static Book[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static Book[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ */
+final class BookFactory extends ModelFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function getDefaults(): array
+    {
+        return [
+            'author' => self::faker()->text(255),
+            'price' => self::faker()->randomFloat(),
+            'publishedAt' => \DateTimeImmutable::createFromMutable(self::faker()->dateTime()),
+            'title' => self::faker()->text(255),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): self
+    {
+        return $this
+            // ->afterInstantiate(function(Book $book): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Book::class;
+    }
+}
